55f9632c6c9c7b40791ac70f490d64bae02245f6
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-07-07 17:22:27 +0500

    RoleController yaratildi

---
+++ BookShop/Controllers/RoleController.cs
@@ -0,0 +1,33 @@
+﻿using BookShop.Data;
+using BookShop.Dto;
+using BookShop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Controllers
+{
+    [Route("api/role")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public RoleController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpPost("/save")]
+        public IActionResult SaveRole([FromBody] Role roleDto)
+        {
+            if (roleDto == null)
+            {
+                return BadRequest("null keldi");
+            }
+
+            _dataContext.Roles.Add(roleDto);
+            _dataContext.SaveChanges();
+            return Ok("Saqlandi");
+        }
+    }
+}
---
+++ BookShop/Dto/RoleDto.cs
@@ -0,0 +1,19 @@
+﻿using BookShop.Models;
+
+namespace BookShop.Dto
+{
+    public class RoleDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<Permission> Permissions { get; set; }
+
+        public RoleDto(string name, string description, List<Permission> permissions)
+        {
+            Name = name;
+            Description = description;
+            Permissions = permissions;
+        }
+    }
+}
